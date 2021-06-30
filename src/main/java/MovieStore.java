7b0945c2ad705d587efbedb1ab80e5ac23824a1e
Manjooth <manjoothkler@gmail.com>
2021-06-30 20:41:00 +0100

    Do The Simplest Thing Possible

--- src/main/java/MovieStore.java
+++ src/main/java/MovieStore.java
@@ -1,9 +1,11 @@
 import model.Movie;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class MovieStore {
+    // do the minimum amount to make the test pass
     public List<Movie> findByPartialTitle(String partialTitle) {
-        return null;
+        return new LinkedList<Movie>();
     }
 }
