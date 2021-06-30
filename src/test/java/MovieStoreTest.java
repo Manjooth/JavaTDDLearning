7f28c2057a726a488e491b5c42f21df2e3877f7d
Manjooth <manjoothkler@gmail.com>
2021-06-30 21:33:12 +0100

    tidying up test code

--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -1,4 +1,5 @@
 import model.Movie;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -9,6 +10,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 // first thing: Does Nothing Test
 // what about null checks? Depends on info given - shouldn't really by passing nulls around (data coming in is accurate and ok)
 public class MovieStoreTest {
+    private final MovieStore movieStore = new MovieStore();
+
+    private final Movie harryPotter = new Movie("Harry Potter");
+    private final Movie lordOfTheRings = new Movie("Lord Of The Rings");
+    private final Movie lordOfTheFlies = new Movie("lord of the flies");
+
     @Test
     public void returnsNoResultsWhenNoTitlesPartiallyMatchSearch() throws Exception {
         MovieStore movieStore = new MovieStore();
@@ -18,21 +25,18 @@ public class MovieStoreTest {
         assertThat(results.size(), is(0));
     }
 
+    @Before
+    public void setUp() throws Exception {
+        movieStore.add(harryPotter);
+        movieStore.add(lordOfTheRings);
+        movieStore.add(lordOfTheFlies);
+    }
+
     @Test
     public void findsMoviesWhenTitlesArePartiallyMatched() throws Exception {
+
         // should ask if this case sensitive - deciding its case insensitive
         // do we care about the order?
-        MovieStore movieStore = new MovieStore();
-        Movie harryPotter = new Movie("Harry Potter");
-        movieStore.add(harryPotter);
-        movieStore.add(new Movie("Shawshank Redemption"));
-        movieStore.add(new Movie("Fight Club"));
-        movieStore.add(new Movie("Pacific Rim"));
-        Movie lordOfTheRings = new Movie("Lord Of The Rings");
-        movieStore.add(lordOfTheRings);
-        Movie lordOfTheFlies = new Movie("lord of the flies");
-        movieStore.add(lordOfTheFlies);
-
         List<Movie> results = movieStore.findByPartialTitle("lord");
 
         assertThat(results.size(), is(2));
