0e5fc97a3dcdd89a5440e0acb57977ac5ac7c58e
Manjooth <manjoothkler@gmail.com>
2021-06-30 20:38:59 +0100

    The Do Nothing Test

--- src/test/java/MovieStoreTest.java
+++ src/test/java/MovieStoreTest.java
@@ -1,7 +1,19 @@
+import model.Movie;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 // first thing: Does Nothing Test
 public class MovieStoreTest {
     @Test
-    public void name() throws Exception {}
+    public void returnsNoResultsWhenNoTitlesPartiallyMatchSearch() throws Exception {
+        MovieStore movieStore = new MovieStore();
+
+        List<Movie> results = movieStore.findByPartialTitle("abc");
+
+        assertThat(results.size(), is(0));
+    }
 }
