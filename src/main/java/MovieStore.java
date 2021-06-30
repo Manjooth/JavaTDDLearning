7efdbdeee4d1841bc46bb14e85a1bbf491d3bbf3
Manjooth <manjoothkler@gmail.com>
2021-06-30 22:21:35 +0100

    using interface to tidy code step 1

--- src/main/java/MovieStore.java
+++ src/main/java/MovieStore.java
@@ -7,25 +7,35 @@ public class MovieStore {
     // ask how many movies are we expecting to store
     List<Movie> movies = new LinkedList<Movie>();
 
+    public void add(Movie movie) {
+        movies.add(movie);
+    }
+
     // do the minimum amount to make the test pass
     public List<Movie> findByPartialTitle(String partialTitle) {
         List<Movie> movieList = new LinkedList<Movie>();
         for (Movie movie : movies) {
-            if(movie.title().toLowerCase().contains(partialTitle.toLowerCase())){
+            if(new Predicate() {
+                @Override
+                public boolean matches(Movie movie) {
+                    return movie.title().toLowerCase().contains(partialTitle.toLowerCase());
+                }
+            }.matches(movie)){
                 movieList.add(movie);
             }
         }
         return movieList;
     }
 
-    public void add(Movie movie) {
-        movies.add(movie);
-    }
-
     public List<Movie> findDirector(String director) {
         List<Movie> movieList = new LinkedList<Movie>();
         for (Movie movie : movies) {
-            if(movie.director().equals(director)){
+            if(new Predicate() {
+                @Override
+                public boolean matches(Movie movie) {
+                    return movie.director().equals(director);
+                }
+            }.matches(movie)){
                 movieList.add(movie);
             }
         }
@@ -35,11 +45,20 @@ public class MovieStore {
     public List<Movie> findByReleaseYear(int from, int to) {
         List<Movie> movieList = new LinkedList<Movie>();
         for (Movie movie : movies) {
-            if(movie.year() <= to && movie.year() >= from){
+            if(new Predicate() {
+                @Override
+                public boolean matches(Movie movie) {
+                    return movie.year() <= to && movie.year() >= from;
+                }
+            }.matches(movie)){
                 movieList.add(movie);
             }
         }
 
         return movieList;
+    }
+
+    interface Predicate {
+        boolean matches(Movie movie);
     }
 }
